cc03906fe8b608bc27747df234a86ff8559ffedd
Jgelks <168496651+Jgelks@users.noreply.github.com>
2024-09-22 23:39:01 -0500

    Add files via upload

---
+++ rules_test.cpp
@@ -0,0 +1,63 @@
+#include <cassert>
+#include <iostream>
+
+#include "board.hpp"
+#include "rules.hpp"
+
+using namespace std;
+
+int main() {
+    cout << "Starting the tests" << endl;
+
+    Board board;
+    Rules *rules = new Rules(&board);
+
+    assert(rules->inProgress());
+
+    board.move(1, "X");
+    assert(rules->inProgress());
+
+    board.move(2, "X");
+    assert(rules->inProgress());
+
+    board.move(3, "X");
+    assert(!rules->inProgress());
+
+    board.clear();
+
+    board.move(1, "O");
+    assert(rules->inProgress());
+
+    board.move(2, "O");
+    assert(rules->inProgress());
+
+    board.move(3, "O");
+    assert(!rules->inProgress());
+
+    board.clear();
+
+    board.move(1, "F");
+    assert(rules->inProgress());
+    assert(rules->status() == "");
+
+    board.move(4, "F");
+    assert(rules->inProgress());
+    assert(rules->status() == "");
+
+    board.move(7, "F");
+    assert(!rules->inProgress());
+    assert(rules->status() == "F");
+
+    board.clear();
+
+    board.move(2, "P");
+    assert(rules->inProgress());
+
+    board.move(5, "P");
+    assert(rules->inProgress());
+
+    board.move(8, "P");
+    assert(!rules->inProgress());
+
+    cout << "All of the tests passed!" << endl;
+}
